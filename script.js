6e11ed268a04c4b7da30e3497bbc46f5bf1f3faf
Bart Gloyd <bartgloyd@gmail.com>
2021-12-27 13:26:22 -0500

    made prompts

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 var generateBtn = document.querySelector("#generate");
 
 
-
-prompt("Choose a length between 8-128", 8-128);
-
-
 function generatePassword() {
-  var value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*/";
-  var valuelength = "8-128";
+  console.log('Hey, YOu clicked');  
+
   
+  var criteria = prompt('Set the length of your password', '8-128');
+  var uppercase = prompt('Do you want to include Uppercase?', "Yes or no?");
+  var lowercase = prompt("Do you want to include lowercase?", "Yes or No?");
+  var numbers = prompt('Do you want to include numbers?', "Yes or no?");
+  var characters = prompt ('Do you want to include special characters?', "Yes or No?");
+  
+  
+
 
 
 }
